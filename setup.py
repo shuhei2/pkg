76317d6929c4fef8ccc9e389dbf67f33bcd3ca32
Your Name <you@example.com>
2022-05-19 01:15:34 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+ 
+setuptools.setup(
+    name="pkg",
+    version="1.0",
+    author="kemi",
+    author_email="kemi@example.com",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://example.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[]
+)
