a500f4cd8e175ff57decd40c67bb9e3c7c9d290c
Your Name <you@example.com>
2022-05-19 10:45:36 +0900

    no message

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -1,4 +1,4 @@
 import requests
 
-with open("temp.txt", "w") as f:
-    f.write(requests.get("https://www.yahoo.co.jp").content.decode(), encoding="utf-8")
+with open("temp.txt", "w", encoding="utf-8") as f:
+    f.write(requests.get("https://www.yahoo.co.jp").content.decode())
